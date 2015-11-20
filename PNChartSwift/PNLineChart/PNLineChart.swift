2997d6cc5cf3d389982ac19f51a6c307ae0595da
Brian Vallelunga <vallelungabrian@gmail.com>
2015-11-19 17:48:41 -0800

    Update PNLineChart.swift

--- PNChartSwift/PNLineChart/PNLineChart.swift
+++ PNChartSwift/PNLineChart/PNLineChart.swift
@@ -539,7 +539,8 @@ public class PNLineChart: UIView{
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        setDefaultValues()
     }
 
 }
